9c1e0a22bfc63f4de94e2632102ed02f9fd0db79
Arkaitz Garro <arkaitz.garro@gmail.com>
2018-05-08 15:30:42 +0200

    chore: add integration tests (#23)

--- example/create-transfer.js
+++ example/create-transfer.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const createWTClient = require('../src');
+const createWTClient = require('@wetransfer/js-sdk');
 
 function readFile(path) {
   return new Promise((resolve, reject) => {
@@ -17,38 +17,38 @@ function readFile(path) {
 
 const data = {
   transfer: {
-    name: 'WeTransfer SDK'
+    name: 'Japan 🇯🇵'
   },
   items: [
     {
-      local_identifier: 'WeTransfer-01.jpg',
+      local_identifier: 'Japan-01.jpg',
       content_identifier: 'file',
-      filename: 'WeTransfer-01.jpg',
-      path: path.join(__dirname, './files/WeTransfer-01.jpg')
+      filename: 'Japan-01.jpg',
+      path: path.join(__dirname, './files/Japan-01.jpg')
     },
     {
-      local_identifier: 'WeTransfer-02.jpg',
+      local_identifier: 'Japan-02.jpg',
       content_identifier: 'file',
-      filename: 'WeTransfer-02.jpg',
-      path: path.join(__dirname, './files/WeTransfer-02.jpg')
+      filename: 'Japan-02.jpg',
+      path: path.join(__dirname, './files/Japan-02.jpg')
     },
     {
-      local_identifier: 'WeTransfer-03.jpg',
+      local_identifier: 'Japan-03.jpg',
       content_identifier: 'file',
-      filename: 'WeTransfer-03.jpg',
-      path: path.join(__dirname, './files/WeTransfer-03.jpg')
+      filename: 'Japan-03.jpg',
+      path: path.join(__dirname, './files/Japan-03.jpg')
     },
     {
-      local_identifier: 'WeTransfer-04.jpg',
+      local_identifier: 'Japan-04.jpg',
       content_identifier: 'file',
-      filename: 'WeTransfer-04.jpg',
-      path: path.join(__dirname, './files/WeTransfer-04.jpg')
+      filename: 'Japan-04.jpg',
+      path: path.join(__dirname, './files/Japan-04.jpg')
     },
     {
-      local_identifier: 'WeTransfer-05.jpg',
+      local_identifier: 'Japan-05.jpg',
       content_identifier: 'file',
-      filename: 'WeTransfer-05.jpg',
-      path: path.join(__dirname, './files/WeTransfer-05.jpg')
+      filename: 'Japan-05.jpg',
+      path: path.join(__dirname, './files/Japan-05.jpg')
     }
   ]
 };
@@ -59,9 +59,7 @@ const data = {
   );
 
   try {
-    const apiClient = await createWTClient(
-      '/* Your private API KEY goes here */'
-    );
+    const apiClient = await createWTClient(process.env.WT_API_KEY);
 
     const transfer = await apiClient.transfer.create(data.transfer);
     const transferItems = await apiClient.transfer.addItems(
