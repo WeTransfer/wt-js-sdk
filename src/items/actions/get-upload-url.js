9134dec3926b02c5f263c84c4ea5961edd1e9c16
Arkaitz Garro <arkaitz.garro@gmail.com>
2018-07-23 14:06:19 +0200

    Update method documentation

--- src/items/actions/get-upload-url.js
+++ src/items/actions/get-upload-url.js
@@ -4,8 +4,8 @@ module.exports = function({ request, routes }) {
   /**
    * Get an upload URL for an specific file part
    * @param   {Object}  file       Item containing information about number of parts, upload url, etc.
-   * @param   {Number}  partNumber A collection of items to be added to the transfer
-   * @returns {Promise}            A collection of created items
+   * @param   {Number}  partNumber Which part number to upload
+   * @returns {Promise}            An object containing the upload url, expiring date, etc
    */
   return async function getUploadURL(file, partNumber) {
     logger.info(
