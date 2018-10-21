707c1c693e32bdd9017a3dfd2676e67c330cc627
Arkaitz Garro <arkaitz.garro@gmail.com>
2018-10-21 19:57:39 +0200

    chore: reduce complexity of transfer create action

--- src/transfers/actions/create.js
+++ src/transfers/actions/create.js
@@ -24,6 +24,27 @@ module.exports = function({
   }
 
   /**
+   * Given the content of the files and a remote transfer,
+   * upload all the files and finalize the transfer.
+   * @param   {Array}   filesContent   An array containing the content of each file
+   * @param   {Object}  remoteTransfer A transfer object
+   * @returns {Promise}                A transfer object
+   */
+  async function uploadFilesAndFinalize(filesContent, remoteTransfer) {
+    await Promise.all(
+      remoteTransfer.files.map((file) => {
+        return uploadFileToTransfer(
+          remoteTransfer,
+          file,
+          filesContent[file.name]
+        );
+      })
+    );
+
+    return await finalizeTransfer(remoteTransfer);
+  }
+
+  /**
    * Creates a new transfer
    * @param   {Object}  transfer A transfer object containing files.
    * @returns {Promise}          A transfer object
@@ -41,19 +62,14 @@ module.exports = function({
       let remoteTransfer = new RemoteTransfer(response);
 
       // If the files array contains the content of the file
-      // lets upload directly, without asking the user to do it.
+      // lets upload directly, without asking the user to do it,
+      // and finalize the transfer in one go.
       if (shouldUploadFiles(transfer.files)) {
         const filesContent = contentForFiles(transfer.files);
-        await Promise.all(
-          remoteTransfer.files.map((file) => {
-            return uploadFileToTransfer(
-              remoteTransfer,
-              file,
-              filesContent[file.name]
-            );
-          })
+        remoteTransfer = await uploadFilesAndFinalize(
+          filesContent,
+          remoteTransfer
         );
-        remoteTransfer = await finalizeTransfer(remoteTransfer);
       }
 
       return remoteTransfer;
