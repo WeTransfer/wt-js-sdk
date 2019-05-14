c1fb74b656bd5d0f9005db7a1239928c3ee3c1d2
Arkaitz Garro <arkaitz.garro@gmail.com>
2019-05-14 11:19:19 +0200

    Reduce chunk queue complexity

--- src/actions/queues/chunks-queue.js
+++ src/actions/queues/chunks-queue.js
@@ -3,22 +3,22 @@ const queue = require('async/queue');
 const config = require('../../config');
 const logger = require('../../config/logger');
 
-function canRetryUpload(chunk, config) {
-  return chunk.retries < config.chunkRetries;
-}
-
-function retryUpload(chunk, callback, config) {
-  chunk.retries++;
-  return setTimeout(
-    () => enqueueChunk(chunk, callback),
-    config.chunkRetryDelay
-  );
-}
-
 module.exports = function({ uploadChunk }) {
   // Create a queue object with concurrency of 5 (default).
   // uploadChunk will be executed for every chunk.
   const uploadQueue = queue(uploadChunk, config.concurrency);
+
+  function canRetryUpload(chunk, config) {
+    return chunk.retries < config.chunkRetries;
+  }
+
+  function retryUpload(chunk, callback, config) {
+    chunk.retries++;
+    return setTimeout(
+      () => enqueueChunk(chunk, callback),
+      config.chunkRetryDelay
+    );
+  }
 
   return function enqueueChunk(chunk, callback) {
     logger.debug(
